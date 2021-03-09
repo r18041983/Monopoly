f3f221f399e7044f9b06d66f1ce33aba42f4346e
Rodion Molchanov <r18041983@gmail.com>
2021-03-09 19:24:37 +0700

    Initial Commit

---
+++ Monopoly/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monopoly
+//
+//  Created by Rodion Molchanov on 09.03.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
